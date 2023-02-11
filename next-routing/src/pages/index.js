b20d19a2f245d6397cd793383436119666b26f4e
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2023-02-11 23:47:15 +0600

    commit

--- next-routing/src/pages/index.js
+++ next-routing/src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -13,7 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <p>Hello</p>
+        <Link href="/"><h2>Home Page</h2></Link>
+        <Link href="/blog"><h3>Blog page</h3></Link>
+        <Link href="/product"><h3>Product page</h3></Link>
       </div>
     </>
   )
