fa9884ca2031437cd0329e8ff17b1056f71b2714
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2023-02-12 01:46:09 +0600

    Navigate through link and programmatically

--- next-routing/src/pages/product/[productId].js
+++ next-routing/src/pages/product/[productId].js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 export default function ProductDetails() {
-    let {productId} = useRouter().query;
+  let { productId } = useRouter().query;
+  let router = useRouter();
   return (
-    <div>ProductDetails {productId}</div>
+    <>
+      <div>ProductDetails {productId}</div>
+      <p onClick={() => { router.push('/product') }} style={{'cursor':'pointer'}}>Go back</p>
+    </>
   )
 }
--- next-routing/src/pages/product/index.js
+++ next-routing/src/pages/product/index.js
@@ -1,5 +1,11 @@
+import Link from "next/link"
 export default function Product() {
+  const prodId = 100
   return (
-    <div>Product</div>
+    <div>
+      <Link href="/">Home</Link>
+      <Link href="/product/1"><h1>Product 1</h1></Link>
+      <Link href={'/product/'+prodId} replace><h1>Product 100</h1></Link>
+    </div>
   )
 }
